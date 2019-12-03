3f93f1b63dc1c9b5434cb951c31fee471783dfee
salehudeen <49670748+salehudeen@users.noreply.github.com>
2019-12-03 16:25:37 +0300

    xhjfjcf

---
+++ testing c on vs code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    int number,n,sum;
+    printf("PLEASE INPUT A DIGIT FOR SUMATION");
+    scanf("%d",number);
+    printf("next number");
+    scanf("%d",n);
+    sum = number + n;
+    printf("%d",sum);
+}
